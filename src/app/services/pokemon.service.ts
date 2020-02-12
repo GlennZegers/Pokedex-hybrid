7a1f6035c1ee0e5ded785df7b157697584ee236d
Unknown <gtmzeger@avans.nl>
2020-02-12 15:33:34 +0100

    Added a start

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {map} from 'rxjs/operators'
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class PokemonService{
+    baseURL = "https://pokeapi.co/api/v2"
+    imageURL= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+    constructor(private http: HttpClient){
+
+    }
+
+    getPokemon(offset = 0){
+        let httpOptions = {
+            headers: new HttpHeaders({
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin': '*'
+            }),
+          };
+        return this.http.get(`${this.baseURL}/pokemon?offset=${offset}&limit=25`, httpOptions).pipe(
+            map(result=>{
+                return result['results']
+            }),
+            map(pokemons=>{
+                 return pokemons.map((pokemon,index) =>{
+                    pokemon.image= this.getPokemonImage(index + offset + 1)
+                    pokemon.pokeIndex = index + offset + 1;
+                    return pokemon;
+                })
+            })
+        )
+    }
+
+    getPokemonImage(index){
+        return `${this.imageURL}${index}.png`
+    }
+}
---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+import { IonInfiniteScroll } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit{
+  offset = 0;
+  pokemon = [];
+  @ViewChild(IonInfiniteScroll, {static: false}) infinite: IonInfiniteScroll;
+
+  constructor(private pokeService: PokemonService) {}
+
+  ngOnInit(){
+    this.loadPokemon();
+  }
+
+  loadPokemon(loadMore = false, event?){
+    if(loadMore){
+      this.offset += 25
+    }
+    this.pokeService.getPokemon(this.offset).subscribe(res=>{
+      this.pokemon = [...this.pokemon, ...res]
+    })
+
+    if(event){
+      event.target.complete();
+    }
+
+    if(this.offset === 125){
+      this.infinite.disabled = true;
+    }
+  }
+}
