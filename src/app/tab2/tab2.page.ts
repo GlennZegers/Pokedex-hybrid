27e83e6d2d20b049de1c56b8908d23699336bcd1
annadame <ajdame@avans.nl>
2020-03-05 15:32:38 +0100

    Tested some things with leaflet

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
 import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+import { Observable } from 'rxjs';
 
 @Component({
 	selector: 'app-tab2',
@@ -9,19 +10,33 @@ import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
 	styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-	latitude = 0;
-	longitude = 0;
+	latitude;
+	longitude;
 	map: Map;
+	pokemonCoords = [
+		{ 'Latitude': 51.824889999999996, 'Longtitude': 4.8799534, 'Pokemon': 'Snorlax' },
+		{ 'Latitude': 51.82490070000001, 'Longtitude': 4.880012499999999, 'Pokemon': 'Pikachu' }
+	]
 
 	constructor(private geolocation: Geolocation, private nativeGeocoder: NativeGeocoder) {
 		//this.getCurrentLocation();
 		this.watchLocation();
 	}
 
-	ionViewDidEnter() { this.loadmap(); }
+	ionViewDidEnter() {
+		// this.watchLocation().subscribe((data) => {
+		// 	console.log(data)
+		// 	this.loadmap(data.latitude, data.longitude);
+		// })
+		this.loadmap();
+	}
 
-	loadmap() {
-		this.map = new Map("map").setView([46.879966, -121.726909], 7);
+	loadmap() { //(latitude: number, longitude: number)
+		// while (this.latitude == null || this.longitude == null) {
+
+		// }
+		//this.map = new Map('map').setView([latitude, longitude], 7);
+		this.map = new Map('map').setView([46.879966, -121.726909], 7);
 
 		tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 			attribution:
@@ -37,16 +52,31 @@ export class Tab2Page {
 		});
 	}
 
-	watchLocation() {
+		watchLocation() { //: Observable<Coordinates> => {
 		let watch = this.geolocation.watchPosition();
 		watch.subscribe((data) => {
 			this.latitude = data.coords.latitude
 			this.longitude = data.coords.longitude
-			console.log(this.latitude)
-			console.log(this.longitude)
+
+			this.pokemonCoords.forEach(coord => {
+				if (coord.Latitude == this.latitude && coord.Longtitude == this.longitude) {
+					var today = new Date();
+					var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+					console.log(time + " " + coord.Pokemon)
+				}
+			});
+
+			// console.log(this.latitude)
+			// console.log(this.longitude)
 
 			this.convertGeocodeToAdress(data.coords.latitude, data.coords.latitude)
+
+			console.log("return coords")
+			return data.coords;
 		});
+
+		console.log("return null")
+		//return null;
 	}
 
 	convertGeocodeToAdress(latitude, longitude) {
