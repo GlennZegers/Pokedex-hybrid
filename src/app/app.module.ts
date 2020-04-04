8796f24214b80813fbab82b3ee2d849cd4b08b70
annadame <ajdame@avans.nl>
2020-04-04 16:33:29 +0200

    Added push notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { IonicStorageModule } from '@ionic/storage';
 import {HttpClientModule} from '@angular/common/http';
 
 import { Camera } from '@ionic-native/camera/ngx';
+import { OneSignal } from '@ionic-native/onesignal/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,7 +24,8 @@ import { Camera } from '@ionic-native/camera/ngx';
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    Camera
+    Camera,
+    OneSignal
   ],
   bootstrap: [AppComponent]
 })
